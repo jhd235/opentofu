7c90a5168e51f5d98f95e580ad099c9e3f57f6f6
jhd235 <105744886+jhd235@users.noreply.github.com>
2024-07-18 16:21:19 +0500

    update main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,8 @@ resource "azurerm_resource_group" "example" {
   name     = "myResGroup"
   location = "West Europe"
 }
+
+output "resource_group_name" {
+  description = "The name of the Azure Resource Group"
+  value       = azurerm_resource_group.example.name
+}
